544d751ea1edf0638e7996d241226c317ba6cf38
SimJoonTTMD <wesley0940@gmail.com>
2020-11-30 21:35:18 +0900

    restore

--- crudapp/static/scriptjs.js
+++ crudapp/static/scriptjs.js
@@ -1,15 +1,16 @@
 var randomNumber = 0;
 var box = document.getElementById("box");
-
+console.log(box.clientWidth());
 function change() {
 
-    randomNumber = Math.floor(Math.random() * 1000) + 1;
+    randomNumber = Math.floor(Math.random() * 800) + 128;
 
     loc.style.top = randomNumber; 
     document.getElementById("x").innerHTML = randomNumber;
 
-    randomNumber = Math.floor(Math.random() * 1000) + 1;
+    randomNumber = Math.floor(Math.random() * 1600) + 50;
 
     loc.style.left = randomNumber;
     document.getElementById("y").innerHTML = randomNumber;
+    console.log("corres");
 }
